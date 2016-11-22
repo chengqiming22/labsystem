6715001607fdb009369dc3a64b8c5fdb4ff5b711
qmcheng <qmcheng@Ctrip.com>
2016-11-22 19:14:02 +0800

    测试

---
+++ account/persist/src/main/java/com/cqm/labsystem/account/persist/AccountPersistException.java
@@ -0,0 +1,10 @@
+package com.cqm.labsystem.account.persist;
+
+/**
+ * Created by qmcheng on 2016/11/21 0021.
+ */
+public class AccountPersistException extends Exception {
+    public AccountPersistException(Exception innerException){
+        super(innerException);
+    }
+}
